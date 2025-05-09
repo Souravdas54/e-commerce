424269fd683ec77bfc3a145f583208c132c8f1ad
Sourav Das <sd.sourav54@gmail.com>
2025-05-10 02:40:22 +0530

    comment

--- src/api/axiosinstance.tsx
+++ src/api/axiosinstance.tsx
@@ -1,24 +1,24 @@
 // axiosInstance.ts
 
-import axios, { AxiosRequestConfig } from 'axios';
-import { baseURL } from './endpoint';
+// import axios, { AxiosRequestConfig } from 'axios';
+// import { baseURL } from './endpoint';
 
-export const axiosInstance = axios.create({
-    baseURL,
-});
+// export const axiosInstance = axios.create({
+//     baseURL,
+// });
 
-axiosInstance.interceptors.request.use(
-    (config: AxiosRequestConfig) => {
-        const token = localStorage.getItem('token') || sessionStorage.getItem('token');
-        if (token) {
-            if (!config.headers) config.headers = {};
-            config.headers['x-access-token'] = token;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
+// axiosInstance.interceptors.request.use(
+//     (config: AxiosRequestConfig) => {
+//         const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+//         if (token) {
+//             if (!config.headers) config.headers = {};
+//             config.headers['x-access-token'] = token;
+//         }
+//         return config;
+//     },
+//     (error) => {
+//         return Promise.reject(error);
+//     }
+// );
 
-export default axiosInstance;
+// export default axiosInstance;
